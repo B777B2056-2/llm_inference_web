1cff986b6b8a93d3b73f120492dc586b042ad914
B777B2056-2 <md82b2127@gmail.com>
2025-04-30 15:11:14 +0800

    删除不需要的包

--- online_inference/llm.py
+++ online_inference/llm.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 from typing import List
-import asyncio
-import uuid
-
 from vllm.engine.async_llm_engine import AsyncLLMEngine
 from vllm import SamplingParams, AsyncEngineArgs, TokensPrompt
-
-from transformers import AutoTokenizer
 
 
 class StreamLLMInference(object):
@@ -32,28 +27,3 @@ class StreamLLMInference(object):
       output_length = len(current_token_ids)
       if request_output.finished:
         break
-
-
-if __name__ == "__main__":
-  import config
-  async def main():
-    llm = StreamLLMInference(config.MODEL)
-    request_id = str(uuid.uuid4())
-    prompt = '天空为什么是蓝色的？'
-    tokenizer = AutoTokenizer.from_pretrained(config.MODEL)
-    encoding = tokenizer(prompt)
-    input_ids = encoding.input_ids
-    if "token_type_ids" in encoding:
-      token_type_ids = encoding.token_type_ids
-    else:
-      token_type_ids = None
-
-    sampling_params = SamplingParams(
-      temperature=0.8,
-      max_tokens=100,
-      detokenize=False,
-    )
-    async for token_ids in llm.stream_generation(request_id, sampling_params, input_ids, token_type_ids):
-      print(token_ids, end="", flush=True)
-    print()
-  asyncio.run(main())
